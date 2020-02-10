b9097f2b496deadb4ab375c25ca258084d43e3cd
Philipp <pd95@users.noreply.github.com>
2020-02-10 23:24:37 +0100

    Regroup custom views

---
+++ GHFollower/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,21 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollower
+//
+//  Created by Philipp on 10.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
