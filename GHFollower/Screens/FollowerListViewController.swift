bf83707ce7a0809b7c2abb6187e5bbe55e087ca5
Philipp <pd95@users.noreply.github.com>
2020-02-10 17:35:59 +0100

    Fix navigation bar stuttering

--- GHFollower/Screens/FollowerListViewController.swift
+++ GHFollower/Screens/FollowerListViewController.swift
@@ -13,13 +13,12 @@ class FollowerListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
