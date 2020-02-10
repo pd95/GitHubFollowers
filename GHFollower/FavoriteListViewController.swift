06c4800516ae4b094847d189e983b4ee171c6518
Philipp <pd95@users.noreply.github.com>
2020-02-10 12:30:13 +0100

    App Navigation Setup

---
+++ GHFollower/FavoriteListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteListViewController.swift
+//  GHFollower
+//
+//  Created by Philipp on 10.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollower/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GHFollower
+//
+//  Created by Philipp on 10.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
