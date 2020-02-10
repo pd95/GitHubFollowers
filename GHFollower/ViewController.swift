1e97533ea8935fc0dda200d1249859b450e68c8a
Philipp <pd95@users.noreply.github.com>
2020-02-10 11:47:47 +0100

    Initial Commit

---
+++ GHFollower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollower
+//
+//  Created by Philipp on 10.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
