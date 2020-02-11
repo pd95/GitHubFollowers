481c15b1dbdee9430263494812c2d36391071bde
Philipp <pd95@users.noreply.github.com>
2020-02-11 10:41:21 +0100

    Network call for user info

--- GHFollower/Screens/UserInfoViewController.swift
+++ GHFollower/Screens/UserInfoViewController.swift
@@ -17,10 +17,25 @@ class UserInfoViewController: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissModal))
         navigationItem.rightBarButtonItem = doneButton
+
+        getUserInfo()
     }
     
     @objc func dismissModal() {
         self.dismiss(animated: true)
     }
     
+    
+    private func getUserInfo() {
+        NetworkManager.shared.getUserInfo(for: userName) { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
+
+                case .success(let user):
+                    print(user)
+            }
+        }
+    }
 }
