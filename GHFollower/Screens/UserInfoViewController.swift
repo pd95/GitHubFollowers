371bd0d34ea3364109f7e3f0dce424ae798b0120
Philipp <pd95@users.noreply.github.com>
2020-02-11 11:33:04 +0100

    Child ViewController for user info header

--- GHFollower/Screens/UserInfoViewController.swift
+++ GHFollower/Screens/UserInfoViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
+    let headerView = UIView()
+    
     var userName : String!
     
     override func viewDidLoad() {
@@ -19,12 +21,33 @@ class UserInfoViewController: UIViewController {
         navigationItem.rightBarButtonItem = doneButton
 
         getUserInfo()
+        layoutUI()
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        
+        headerView.backgroundColor = .systemBackground
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dismissModal() {
         self.dismiss(animated: true)
     }
-    
     
     private func getUserInfo() {
         NetworkManager.shared.getUserInfo(for: userName) { [weak self] (result) in
@@ -34,7 +57,9 @@ class UserInfoViewController: UIViewController {
                     self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
 
                 case .success(let user):
-                    print(user)
+                    DispatchQueue.main.async {
+                        self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+                    }
             }
         }
     }
---
+++ GHFollower/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollower
+//
+//  Created by Philipp on 11.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
