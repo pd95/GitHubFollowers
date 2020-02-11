3837316f26f4ccb95cf33a6188b26e33a6851f1f
Philipp <pd95@users.noreply.github.com>
2020-02-11 11:49:42 +0100

    Cleanup and setup user info view controller

--- GHFollower/Screens/UserInfoViewController.swift
+++ GHFollower/Screens/UserInfoViewController.swift
@@ -11,44 +11,28 @@ import UIKit
 class UserInfoViewController: UIViewController {
     
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    
+    var itemViews: [UIView] = []
     
     var userName : String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureViewController()
+        layoutUI()
+
+        getUserInfo()
+    }
+    
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissModal))
         navigationItem.rightBarButtonItem = doneButton
+    }
 
-        getUserInfo()
-        layoutUI()
-    }
-    
-    func layoutUI() {
-        view.addSubview(headerView)
-        
-        headerView.backgroundColor = .systemBackground
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
-        ])
-    }
-    
-    func add(childVC: UIViewController, to containerView: UIView) {
-        addChild(childVC)
-        containerView.addSubview(childVC.view)
-        childVC.view.frame = containerView.bounds
-        childVC.didMove(toParent: self)
-    }
-    
-    @objc func dismissModal() {
-        self.dismiss(animated: true)
-    }
-    
     private func getUserInfo() {
         NetworkManager.shared.getUserInfo(for: userName) { [weak self] (result) in
             guard let self = self else { return }
@@ -62,5 +46,49 @@ class UserInfoViewController: UIViewController {
                     }
             }
         }
+    }
+
+    private func layoutUI() {
+        
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
+        
+        headerView.backgroundColor = .systemBackground
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemBlue
+
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+        
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+        ])
+    }
+    
+    private func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
+    
+    @objc private func dismissModal() {
+        self.dismiss(animated: true)
     }
 }
