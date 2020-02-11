a37f4b6cae36210b7fd3acd3e12685af8c08d3b9
Philipp <pd95@users.noreply.github.com>
2020-02-11 10:26:30 +0100

    Modal presentation of UserInfoViewController

--- GHFollower/Screens/FollowerListViewController.swift
+++ GHFollower/Screens/FollowerListViewController.swift
@@ -19,6 +19,7 @@ class FollowerListViewController: UIViewController {
     var filteredFollowers = [Follower]()
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
 
     var collectionView : UICollectionView!
     var dataSource : UICollectionViewDiffableDataSource<Section, Follower>!
@@ -119,11 +120,23 @@ extension FollowerListViewController: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destinationVC = UserInfoViewController()
+        destinationVC.userName = follower.login
+
+        // present modally
+        present(UINavigationController(rootViewController: destinationVC), animated: true)
+    }
 }
 
 extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text?.lowercased(), !filter.isEmpty else { return }
+        isSearching = true
         filteredFollowers = followers.filter {
             $0.login.lowercased().contains(filter)
         }
@@ -131,6 +144,7 @@ extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelega
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
---
+++ GHFollower/Screens/UserInfoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoViewController.swift
+//  GHFollower
+//
+//  Created by Philipp on 11.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    
+    var userName : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissModal))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissModal() {
+        self.dismiss(animated: true)
+    }
+    
+}
