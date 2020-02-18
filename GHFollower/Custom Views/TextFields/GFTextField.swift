6d467abe7d787e994d88bb43b5e92c92526cbb87
Philipp <pd95@users.noreply.github.com>
2020-02-18 18:21:49 +0100

    Make GFButton and GFTextField IBDesignable to enable usage in storyboard

---
+++ GHFollower/Custom Views/TextFields/GFTextField.swift
@@ -0,0 +1,47 @@
+//
+//  GFTextField.swift
+//  GHFollower
+//
+//  Created by Philipp on 10.02.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        configure()
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth  = 2
+        layer.borderColor  = UIColor.systemGray4.cgColor
+
+        textColor       = .label
+        tintColor       = .label
+        textAlignment   = .center
+        font            = UIFont.preferredFont(forTextStyle: .title2)
+        minimumFontSize = 12
+        adjustsFontSizeToFitWidth = true
+
+        backgroundColor    = .tertiarySystemBackground
+        autocorrectionType = .no
+        returnKeyType      = .go
+        clearButtonMode    = .whileEditing
+        placeholder        = "Enter a username"
+    }
+}
