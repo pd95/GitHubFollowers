5b10f5085664db08201d45b35c20a6095058fc5d
Philipp <pd95@users.noreply.github.com>
2021-11-01 23:36:09 +0100

    Adding APILoader test for `UserInfoRequest`

--- GHFollowerTests/GitHubAPI_UserInfoRequestTests.swift
+++ GHFollowerTests/GitHubAPI_UserInfoRequestTests.swift
@@ -30,36 +30,98 @@ class GitHubAPI_UserInfoRequestTests: XCTestCase {
     }
 
     func test_ParsingValidResponse() throws {
+        let (user, json) = anyUser()
+        let jsonData = makeUserJSON(json)
+        let response = try request.parseResponse(data: jsonData)
+
+        XCTAssertEqual(response, user)
+    }
+
+    func test_APILoaderLoadsUserSuccessfully() {
         let login = "octocat"
-        let avatarURLString = "https://avatars.githubusercontent.com/u/583231?v=4"
+        let (user, json) = anyUser(login: login)
+        let jsonData = makeUserJSON(json)
+
+        let sut = makeSUT()
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.url?.pathComponents.contains(login), true)
+            return (HTTPURLResponse(), jsonData)
+        }
+
+        let expectation = XCTestExpectation(description: "response")
+        sut.loadAPIRequest(requestData: .init(username: login)) { result in
+            XCTAssertEqual(result, .success(user))
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> APIRequestLoader<GitHubAPI.UserInfoRequest> {
+        let request = GitHubAPI.UserInfoRequest()
+
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession(configuration: configuration)
+
+        let loader = APIRequestLoader(apiRequest: request, urlSession: urlSession)
+        addTeardownBlock { [weak loader] in
+            XCTAssertNil(loader, "Loader should have been deallocated. Potential memory leak", file: file, line: line)
+        }
+        return loader
+    }
+
+    private func anyUser(login: String = "octocat") -> (user: User, json: [String: Any]) {
+        let avatarUrl = "https://avatars.githubusercontent.com/u/583231?v=4"
         let name: String? = "The Octocat"
         let location: String? = "San Francisco"
         let bio: String? = nil
         let publicRepos: Int = 8
         let publicGists: Int = 8
-        let htmlUrl: String = "https://github.com/octocat"
+        let htmlUrl: String = "https://github.com/\(login)"
         let following: Int = 9
         let followers: Int = 4110
         let createdAt = Date(timeIntervalSince1970: 1_295_981_076)
 
-        let jsonData = """
-        {
-            "login": "\(login)",
-            "avatar_url": "\(avatarURLString)",
-            "name": "\(name ?? "null")",
-            "location": "\(location ?? "null")",
-            "bio": \(bio ?? "null"),
-            "public_repos": \(publicRepos),
-            "public_gists": \(publicGists),
-            "html_url": "\(htmlUrl)",
-            "following": \(following),
-            "followers": \(followers),
-            "created_at": "2011-01-25T18:44:36Z"
-        }
-        """.data(using: .utf8)!
+        return makeUser(
+            login: login, avatarUrl: avatarUrl, name: name, location: location, bio: bio,
+            publicRepos: publicRepos, publicGists: publicGists, htmlUrl: htmlUrl,
+            following: following, followers: followers, createdAt: createdAt
+        )
+    }
 
-        let response = try request.parseResponse(data: jsonData)
+    private func makeUser(
+        login: String, avatarUrl: String, name: String? = nil, location: String? = nil, bio: String? = nil,
+        publicRepos: Int, publicGists: Int, htmlUrl: String, following: Int, followers: Int,
+        createdAt: Date
+    ) -> (user: User, json: [String: Any]) {
+        let user = User(
+            login: login, avatarUrl: avatarUrl, name: name, location: location, bio: bio,
+            publicRepos: publicRepos, publicGists: publicGists, htmlUrl: htmlUrl,
+            following: following, followers: followers, createdAt: createdAt
+        )
 
-        XCTAssertEqual(response, User(login: login, avatarUrl: avatarURLString, name: name, location: location, bio: bio, publicRepos: publicRepos, publicGists: publicGists, htmlUrl: htmlUrl, following: following, followers: followers, createdAt: createdAt))
+        let userDict: [String: Any?] = [
+            "login": login,
+            "avatar_url": avatarUrl,
+            "name": name,
+            "location": location,
+            "bio": bio,
+            "public_repos": publicRepos,
+            "public_gists": publicGists,
+            "html_url": htmlUrl,
+            "following": following,
+            "followers": followers,
+            "created_at": ISO8601DateFormatter().string(from: createdAt),
+        ]
+
+        let json = userDict.compactMapValues { $0 }
+
+        return (user, json)
+    }
+
+    private func makeUserJSON(_ user: [String: Any]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: user)
     }
 }
