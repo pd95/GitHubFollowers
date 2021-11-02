2d2ec1b349ed1844189121f00997dd48ca70bfda
Philipp <pd95@users.noreply.github.com>
2021-11-02 10:41:21 +0100

    Added two basic End-to-End tests for `NetworkManager`

--- GHFollowerTests/GHFollowerTests.swift
+++ GHFollowerTests/GHFollowerTests.swift
@@ -6,11 +6,42 @@
 //  Copyright © 2021 Philipp. All rights reserved.
 //
 
+@testable import GHFollower
 import XCTest
 
 class GHFollowerTests: XCTestCase {
-    func testMakingURLRequest() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_NetworkManager_getUserInfo() throws {
+        let exp = expectation(description: "Request completes")
+
+        NetworkManager.shared.getUserInfo(for: "octocat") { result in
+            switch result {
+            case let .success(user):
+                XCTAssertEqual(user.login, "octocat")
+                XCTAssertEqual(user.name, "The Octocat")
+
+            case let .failure(error):
+                XCTFail("Unexpected error \(error)")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 3.0)
+    }
+
+    func test_NetworkManager_getFollowers() throws {
+        let exp = expectation(description: "Request completes")
+
+        NetworkManager.shared.getFollowers(for: "octocat", page: 1) { result in
+            switch result {
+            case let .success(followers):
+                XCTAssert(followers.count > 0, "Expected followers for, but found \(followers)")
+
+            case let .failure(error):
+                XCTFail("Unexpected error \(error)")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 3.0)
     }
 }
